1d4340215aa4945702e30cd3edfa24e40cf9bd2e
David Tran <trandavidq@gmail.com>
2021-11-03 17:07:41 -0400

    Create login form

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header('./views/login.php') ?>
---
+++ views/login.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="./styles/login.css">
+  <meta name="description" content="My Page Description">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+  <h1>Notemates Login</h1>
+  <div id="login-form">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Username</label>
+        <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+      </div>
+      <button type="submit" class="btn btn-primary" style="margin-top: 50px;">Submit</button>
+    </form>
+  </div>
+</body>
+
+</html>
