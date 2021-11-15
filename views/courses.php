1bc0a90ffdd372542eb19af48995facdb182b30b
David Tran <trandavidq@gmail.com>
2021-11-14 20:24:51 -0500

    Skeleton courses page

---
+++ views/courses.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <title>Course Search</title>
+  <meta name="Courses" content="Page should allow for searching of courses">
+  <link rel="stylesheet" href="../styles/courses.css">
+</head>
+    
+<body>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Notemates</span>
+  </div>
+</nav>
+<h1 class="mx-3"> Courses </h1>
+<div class="searchbar">
+    <form action="" method="POST">
+        <h3 style="margin-right: 100px;">Course:</h3>
+        <input type="text" name="course_name">
+
+        <button class="btn btn-success" type="submit">Add</button>
+
+    </form>
+</div>
+<hr>
+</body>
+
+<footer>
+    <div class="card fixed-bottom">
+        <div class="card-header">
+            <small> &copy; Copyright 2021, Notemates Team. All Rights Reserved</small>
+        </div>
+    </div>
+
+</footer>
+
+</html>
