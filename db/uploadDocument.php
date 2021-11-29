ffbf3dd3735fe3cdb9ad09c1ddbe74cd47fdbc95
Ruthvik Gajjala <ruthvikgajjala14@gmail.com>
2021-11-28 22:24:15 -0500

    Added transaction to docUpload

--- db/uploadDocument.php
+++ db/uploadDocument.php
@@ -26,26 +26,33 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 		||$ext=="pdf"||$ext=="PDF"||$ext=="doc"||$ext=="DOC"||$ext=="docx"||$ext=="DOCX"
 		||$ext=="XLS"||$ext=="xls"||$ext=="XLSX"||$ext=="xlsx"||$ext=="xlsm"||$ext=="XLSM"){
 
-    //Insert Document Information
-    $doc_insert_query = "INSERT INTO Document(userID, dateCreated, displayName, fileType, fileName) VALUES ($userid, '$date', '$docName', '$file_type', '$filename')";
-    $result = mysqli_query($conn, $doc_insert_query) or die('Error, doc insert query failed');
+    mysqli_begin_transaction($conn);
+    try{
+      //Insert Document Information
+      $doc_insert_query = "INSERT INTO Document(userID, dateCreated, displayName, fileType, fileName) VALUES ($userid, '$date', '$docName', '$file_type', '$filename')";
+      $result = mysqli_query($conn, $doc_insert_query) or die('Error, doc insert query failed');
 
-    $currentDocID = mysqli_insert_id($conn);
+      $currentDocID = mysqli_insert_id($conn);
 
-    //Need to insert file contents into the File Table
+      //Need to insert file contents into the File Table
 
-    //first find if it's already in the file table
-    $search_file_query = "SELECT * FROM File WHERE fileName = '$filename'";
-    $search_result = mysqli_query($conn, $search_file_query);
-    if($search_result->num_rows == 0) {
-      $contents_insert_query = "INSERT INTO File(fileName, fileType, fileContents) VALUES ('$filename', '$file_type', '$content')";
-      $result2 = mysqli_query($conn, $contents_insert_query) or die('Error, contents query failed');
+      //first find if it's already in the file table
+      $search_file_query = "SELECT * FROM File WHERE fileName = '$filename'";
+      $search_result = mysqli_query($conn, $search_file_query);
+      if($search_result->num_rows == 0) {
+        $contents_insert_query = "INSERT INTO File(fileName, fileType, fileContents) VALUES ('$filename', '$file_type', '$content')";
+        $result2 = mysqli_query($conn, $contents_insert_query) or die('Error, contents query failed');
+      }
+
+      //Need to insert course information into the belongs_to table
+      $course_insert_query = "INSERT INTO belongs_to(userID, documentID, courseID) VALUES ($userid,$currentDocID,'$courseID')";
+      $result3 = mysqli_query($conn, $course_insert_query) or die('Error, belongs query failed');
+      mysqli_commit($conn);
+    } catch (mysqli_sql_exception $exception) {
+      mysqli_rollback($conn);
+      throw $exception;
     }
-
-    //Need to insert course information into the belongs_to table
-    $course_insert_query = "INSERT INTO belongs_to(userID, documentID, courseID) VALUES ($userid,$currentDocID,'$courseID')";
-    $result3 = mysqli_query($conn, $course_insert_query) or die('Error, belongs query failed');
-    }
+  }
     header("Location: ../views/docsearch.php");
     mysqli_close($conn);
     exit;
